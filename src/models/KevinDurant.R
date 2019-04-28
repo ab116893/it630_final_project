9adb4106f25055f018835fe1ad669ef198d129f0
lrav35 <lrav35@gmail.com>
2019-04-28 14:02:33 -0400

    update

--- src/models/KevinDurant.R
+++ src/models/KevinDurant.R
@@ -15,22 +15,25 @@ fieldgoal = (KDstats$eFG)
 turnover = (KDstats$TOV)
 
 
+##### single time series #######
+
 KD_ts = ts(KDstats[,31], frequency = 365.25, start = 2007)
 KD_ts
+
+#Unable to split into test and train
+KD_train = subset(KD_ts, start = 2007, end = 2014) 
+KD_test = window(goog, start = 2015, end = 2018)
 
 autoplot(KD_ts)
 
 
+###### two seperate time series ###
+
+#Doesnt recognize that they should be combined
 train = ts(KDstats[1:9,31])
 test = ts(KDstats[10:12,31])
 train
-
-
-
-
-
-plot = autoplot(train) + autolayer(autoplot(test))
-plot
+test
 
 autoplot(train, series="points train") +
   autolayer(test, series = "points test")
