c6a36642556e87c9d9e26f0b40a00ba302ed6427
lrav35 <lrav35@gmail.com>
2019-04-28 12:16:31 -0400

    add r

--- src/models/KevinDurant.R
+++ src/models/KevinDurant.R
@@ -1,11 +1,34 @@
 getwd()
 setwd("C:\Anaconda\WorkfolderPython\Final_Project")
+setwd("C:/Users/Luc Ravenelle/Desktop")
+library(xts)
+library(fpp2)
+library(forecast)
 
-KDstats <- read.csv("file.csv", header=TRUE, stringsAsFactors = FALSE)
+KDstats <- read.csv("KD.csv", header=TRUE, stringsAsFactors = FALSE)
 
-#Year = (KDstats$year)
-#Points = (KDstats$PTS)
-##Assists = (KDstats$AST)
-#FieldGoal = (KDstats$eFG%)
-#Turnover = (KDstats$TOV)
+year = (KDstats$year)
+points = (KDstats$PTS)
+assists = (KDstats$AST)
+### change column header ####
+fieldgoal = (KDstats$eFG)
+turnover = (KDstats$TOV)
+
+
+
+
+train = ts(KDstats[1:7,2])
+test = ts(KDstats[8:10,2])
+
+
+
+
+
+
+plot = autoplot(train) + autolayer(autoplot(test))
+plot
+
+autoplot(train, series="points train") +
+  autolayer(test, series = "points test")
+ 
 
