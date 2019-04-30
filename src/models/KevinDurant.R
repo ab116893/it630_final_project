785d7b5c92af5d35bc9b1aef772e440c1255d9d5
lrav35 <lrav35@gmail.com>
2019-04-30 10:46:24 -0400

    final R script for KD PTS

--- src/models/KevinDurant.R
+++ src/models/KevinDurant.R
@@ -17,25 +17,38 @@ turnover = (KDstats$TOV)
 
 ##### single time series #######
 
-KD_ts = ts(KDstats[,31], frequency = 365.25, start = 2007)
+KD_ts = ts(KDstats[,31], frequency = 1, start = 2007)
 KD_ts
 
-#Unable to split into test and train
-KD_train = subset(KD_ts, start = 2007, end = 2014) 
-KD_test = window(goog, start = 2015, end = 2018)
+######## Drift method and Naive Method #########
+KD_train = window(KD_ts, start = 2007, end = 2014) 
+KD_test = window(KD_ts, start = 2014, end = 2019)
 
-autoplot(KD_ts)
+Drift_KD = rwf(KD_train, h = 5, drift = TRUE)
+Naive_KD = naive(KD_train, h = 5)
 
 
-###### two seperate time series ###
+autoplot(KD_train, series="points train") +
+  autolayer(KD_test, series = "points test") +
+  autolayer(Drift_KD, PI = FALSE, series = "Drift") +
+  autolayer(Naive_KD, PI = FALSE, series = "Naive")
 
-#Doesnt recognize that they should be combined
-train = ts(KDstats[1:9,31])
-test = ts(KDstats[10:12,31])
-train
-test
+accuracy(Drift_KD)
+accuracy(Naive_KD)
 
-autoplot(train, series="points train") +
-  autolayer(test, series = "points test")
- 
+
+######## Predict Next Year ############
+
+Drift_KD2020 = rwf(KD_train, h = 6, drift = TRUE)
+Naive_KD2020 = naive(KD_train, h = 6)
+
+
+autoplot(KD_train, series="points train") +
+  autolayer(KD_test, series = "points test") +
+  autolayer(Drift_KD2020, PI = FALSE, series = "Drift") +
+  autolayer(Naive_KD2020, PI = FALSE, series = "Naive")
+
+accuracy(Drift_KD)
+accuracy(Naive_KD)
+
 
