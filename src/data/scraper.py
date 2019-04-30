0ddf068ccfce1a943be41d82487d81f074ffe99a
Andreas Sudarto <jsa1012@wildcats.unh.edu>
2019-04-30 11:16:28 -0400

    Change variable name

--- src/data/scraper.py
+++ src/data/scraper.py
@@ -6,22 +6,24 @@ import pandas as pd
 import re
 
 def data():
-    letter = input("First letter of nba player last name: ")
-    name = input("Name of nba player, first five letter in last name and first two letter of their first (EX:curryst,duranke) : ")
-
-    url = "https://www.basketball-reference.com/players/{}/{}01.html".format(letter,name)
-    response = requests.get(url, headers={'User-Agent': 'Chrome/60.0.3112.113'})
-    soup = BeautifulSoup(response.content, 'html.parser')
-
-    # use findALL() to get the column headers
-    soup.findAll('tr', limit=2)
-    # use getText()to extract the text we need into a list
-    headers = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
-    # avoid the first header row
-    data_rows = soup.findAll('tr')
-    player_stats = [[td.getText() for td in data_rows[i].findAll(['td','th'])] for i in range(len(data_rows))]
-
-    stats = pd.DataFrame(player_stats, columns = headers)
-    stats.head(10)
-    stats.to_csv('../../data/raw/{}.csv'.format(name))
+    try:
+        letter = input("First letter of nba player last name: ")
+        name = input("Name of nba player, first five letter in last name and first two letter of their first (EX:curryst,duranke) : ")
+        
+        url = "https://www.basketball-reference.com/players/{}/{}01.html".format(letter,name)
+        response = requests.get(url, headers={'User-Agent': 'Chrome/60.0.3112.113'})
+        soup = BeautifulSoup(response.content, 'html.parser')
+        # use findALL() to get the column headers
+        soup.findAll('tr', limit=3)
+        # use getText()to extract the text we need into a list
+        headers = [th.getText() for th in soup.findAll('tr', limit=3)[0].findAll('th')]
+        # avoid the first header row
+        data_rows = soup.findAll('tr')
+        player_stats = [[td.getText() for td in data_rows[i].findAll(['td','th'])] for i in range(len(data_rows))]
+        
+        stats = pd.DataFrame(player_stats, columns = headers)
+        stats.head(10)
+        stats.to_csv('../../data/raw/{}.csv'.format(name))
+    except:
+        print("Invalid input or error on the scraper")
 data()
--- src/models/KevinDurant.R
+++ src/models/KevinDurant.R
@@ -5,46 +5,46 @@ library(xts)
 library(fpp2)
 library(forecast)
 
-KDstats <- read.csv("duranke.csv", header=TRUE, stringsAsFactors = FALSE)
+stats <- read.csv("duranke.csv", header=TRUE, stringsAsFactors = FALSE)
 
-year = (KDstats$year)
-points = (KDstats$PTS)
-assists = (KDstats$AST)
+year = (stats$year)
+points = (stats$PTS)
+assists = (stats$AST)
 
-fieldgoal = (KDstats$eFG)
-turnover = (KDstats$TOV)
+fieldgoal = (stats$eFG)
+turnover = (stats$TOV)
 
 
 ##### single time series #######
 
-KD_ts = ts(KDstats[,31], frequency = 1, start = 2007)
-KD_ts
+ts = ts(stats[,31], frequency = 1, start = 2007)
+ts
 
 ######## Drift method and Naive Method #########
-KD_train = window(KD_ts, start = 2007, end = 2014) 
-KD_test = window(KD_ts, start = 2014, end = 2019)
+train = window(ts, start = 2007, end = 2014) 
+test = window(ts, start = 2014, end = 2019)
 
-Drift_KD = rwf(KD_train, h = 5, drift = TRUE)
-Naive_KD = naive(KD_train, h = 5)
+Drift = rwf(train, h = 5, drift = TRUE)
+Naive = naive(train, h = 5)
 
 
-autoplot(KD_train, series="points train") +
-  autolayer(KD_test, series = "points test") +
-  autolayer(Drift_KD, PI = FALSE, series = "Drift") +
-  autolayer(Naive_KD, PI = FALSE, series = "Naive")
+autoplot(train, series="points train") +
+  autolayer(test, series = "points test") +
+  autolayer(Drift, PI = FALSE, series = "Drift") +
+  autolayer(Naive, PI = FALSE, series = "Naive")
 
-accuracy(Drift_KD)
-accuracy(Naive_KD)
+accuracy(Drift)
+accuracy(Naive)
 
 
 ######## Predict Next Year ############
 
-Drift_KD2020 = rwf(KD_train, h = 6, drift = TRUE)
-Naive_KD2020 = naive(KD_train, h = 6)
+Drift_2020 = rwf(train, h = 6, drift = TRUE)
+Naive_2020 = naive(train, h = 6)
 
 
-autoplot(KD_train, series="points train") +
-  autolayer(KD_test, series = "points test") +
+autoplot(train, series="points train") +
+  autolayer(test, series = "points test") +
   autolayer(Drift_KD2020, PI = FALSE, series = "Drift") +
   autolayer(Naive_KD2020, PI = FALSE, series = "Naive")
 
